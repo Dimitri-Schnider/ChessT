b1f2f274cc433f8c526a8fe8f1bb1b6c7ddbaeee
Dimitri Schnider <schnidim@students.zhaw.ch>
2025-06-13 08:55:51 +0200

    Implement Win Animation

---
+++ ChessClient/Pages/Components/GlobalAnimation.razor.cs
@@ -0,0 +1,46 @@
+﻿using ChessClient.State;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Globalization;
+
+namespace ChessClient.Pages.Components
+{
+    public partial class GlobalAnimation : ComponentBase, IDisposable
+    {
+        [Inject]
+        private IUiState UiState { get; set; } = default!;
+
+        private readonly Random _random = new();
+
+        protected override void OnInitialized()
+        {
+            UiState.StateChanged += OnUiStateChanged;
+        }
+
+        private void OnUiStateChanged()
+        {
+            InvokeAsync(StateHasChanged);
+        }
+
+        protected string GetConfettiStyle(int index)
+        {
+            double startX = _random.NextDouble() * 100;
+            double delay = _random.NextDouble() * 2;
+            double duration = 1.5 + _random.NextDouble() * 2.0;
+            double rotation = _random.Next(-360, 360);
+            string color = $"hsl({_random.Next(0, 360)}, 90%, 65%)";
+
+            return $"--start-x: {startX.ToString(CultureInfo.InvariantCulture)}vw; " +
+                   $"--delay: {delay.ToString(CultureInfo.InvariantCulture)}s; " +
+                   $"--duration: {duration.ToString(CultureInfo.InvariantCulture)}s; " +
+                   $"--rotation: {rotation.ToString(CultureInfo.InvariantCulture)}deg; " +
+                   $"--color: {color};";
+        }
+
+        public void Dispose()
+        {
+            UiState.StateChanged -= OnUiStateChanged;
+            GC.SuppressFinalize(this);
+        }
+    }
+}
--- ChessClient/Pages/Components/WinLossModal.razor.cs
+++ ChessClient/Pages/Components/WinLossModal.razor.cs
@@ -11,25 +11,9 @@ namespace ChessClient.Pages.Components
         [Parameter] public EventCallback OnNewGameClicked { get; set; }
         [Parameter] public EventCallback OnDownloadHistoryClicked { get; set; }
 
-        protected bool IsWin => Message.Contains("gewonnen", StringComparison.OrdinalIgnoreCase);
+        protected bool IsWin => !string.IsNullOrEmpty(Message) && Message.Contains("gewonnen", StringComparison.OrdinalIgnoreCase);
         protected string Title => IsWin ? "Sieg!" : "Niederlage";
 
-        private readonly Random _random = new();
-
-        protected string GetConfettiStyle(int index)
-        {
-            double startX = _random.NextDouble() * 100;
-            double delay = _random.NextDouble() * 4;
-            double duration = 2.5 + _random.NextDouble() * 2.5;
-            double rotation = _random.Next(-360, 360);
-            string color = $"hsl({_random.Next(0, 360)}, 90%, 65%)";
-
-            return $"--start-x: {startX}vw; " +
-                   $"--delay: {delay}s; " +
-                   $"--duration: {duration}s; " +
-                   $"--rotation: {rotation}deg; " +
-                   $"--color: {color};";
-        }
 
         private async Task HandleNewGameClicked()
         {
